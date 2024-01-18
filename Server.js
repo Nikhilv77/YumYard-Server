4ee39cbfdd1a0f25d291ecd63507c3e558424457
Nikhil <Nv581339@gmail.com>
2024-01-18 15:36:38 +0530

    fixing errors

--- Server.js
+++ Server.js
@@ -14,10 +14,11 @@ const feedbackRoutes = require('./routes/FeedbackRoutes')
 const donationRoutes = require('./routes/DonationRoutes')
 const sessionOrderRoutes=require('./routes/SessionOrderRoutes')
 const sessionDonationRoutes = require('./routes/SessionDonationRoutes')
-const corse = require('cors')
+const cors = require('cors');
+
 const cookieParser = require('cookie-parser');
 app.use(express.json());
-app.use(corse());
+app.use(cors());
 app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("server working");
