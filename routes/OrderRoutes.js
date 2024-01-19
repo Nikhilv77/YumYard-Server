93a16d9c91e042c18f383b2efb7bfa3233e2bb12
Nikhil <Nv581339@gmail.com>
2024-01-20 02:57:35 +0530

    fixing

--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -9,7 +9,7 @@ const stripe = require("stripe")(
 );
 let newOrder;
 const htmlToPdfBuffer = async (html) => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
   await page.setContent(html);
   const pdfBuffer = await page.pdf({ format: "A4" });
